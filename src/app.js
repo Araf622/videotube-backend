6dc68687ebdc3d43a12125e5e69f91564ca55fcd
araf622 <bsse1220@iit.du.ac.bd>
2025-06-20 00:53:40 +0600

    add refresh token endpoint

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors"
+import cookieParser from "cookie-parser";
 import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express"
 
@@ -38,6 +39,7 @@ app.use(cors())
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
+app.use(cookieParser())
 
 import userRouter from './routes/user.routes.js'
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUSer, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../controllers/auth.middleware.js";
 
 const router = Router()
 
@@ -16,12 +17,17 @@ const router = Router()
 
 router.route("/register").post(
     upload.fields([{
-        name : "avatar",
-        maxCount : 1,
+        name: "avatar",
+        maxCount: 1,
     }, {
-        name : "coverImage",
-        maxCount : 1,
+        name: "coverImage",
+        maxCount: 1,
     }]),
     registerUser)
+
+router.route("/login").post(loginUSer)
+router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
+
 
 export default router
